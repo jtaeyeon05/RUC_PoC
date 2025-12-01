130e7494aeeadf06345c82a24d498ccdc2ee1952
jtaeyeon05 <jtaeyeon05@gmail.com>
2025-12-01 16:27:45 +0900

    Fix: Scripts

--- poc_scripts/test_ruc_edns0.sh
+++ poc_scripts/test_ruc_edns0.sh
@@ -7,35 +7,3 @@ sleep 5
 echo "Tesing BIND resolver against ruc_edns0..."
 docker restart ruc-bind
 docker exec ruc-attacker python3 /root/poc_scripts/ruc_poc.py --resolver_ip 172.22.1.1 --ruc_variant ruc_edns0
-
-# test powerdns
-echo "Preparing nameserver..."
-docker restart ruc-nameserver
-sleep 5
-echo "Tesing PowerDNS resolver against ruc_edns0..."
-docker restart ruc-powerdns
-docker exec ruc-attacker python3 /root/poc_scripts/ruc_poc.py --resolver_ip 172.22.1.2 --ruc_variant ruc_edns0
-
-# test unbound
-echo "Preparing nameserver..."
-docker restart ruc-nameserver
-sleep 5
-echo "Tesing Unbound resolver against ruc_edns0..."
-docker restart ruc-unbound
-docker exec ruc-attacker python3 /root/poc_scripts/ruc_poc.py --resolver_ip 172.22.1.3 --ruc_variant ruc_edns0
-
-# test knot
-echo "Preparing nameserver..."
-docker restart ruc-nameserver
-sleep 5
-echo "Tesing Knot resolver against ruc_edns0..."
-docker restart ruc-knot
-docker exec ruc-attacker python3 /root/poc_scripts/ruc_poc.py --resolver_ip 172.22.1.4 --ruc_variant ruc_edns0
-
-# test technitium
-echo "Preparing nameserver..."
-docker restart ruc-nameserver
-sleep 5
-echo "Tesing Technitium resolver against ruc_edns0..."
-docker restart ruc-technitium
-docker exec ruc-attacker python3 /root/poc_scripts/ruc_poc.py --resolver_ip 172.22.1.5 --ruc_variant ruc_edns0
--- start_resolver.sh
+++ start_resolver.sh
@@ -2,16 +2,8 @@
 
 # build docker images
 docker build -f dockers/resolver_software/bind.Dockerfile -t ruc-bind:9.20.3 .
-docker build -f dockers/resolver_software/powerdns.Dockerfile -t ruc-powerdns:5.1.3 .
-docker build -f dockers/resolver_software/unbound.Dockerfile -t ruc-unbound:1.22.0 .
-docker build -f dockers/resolver_software/knot.Dockerfile -t ruc-knot:5.7.4 .
-docker build -f dockers/resolver_software/technitium.Dockerfile -t ruc-technitium:13.1 .
 
 # start docker containers
 docker run -d --name ruc-bind --network ruc-test-net --ip 172.22.1.1 ruc-bind:9.20.3
-docker run -d --name ruc-powerdns --network ruc-test-net --ip 172.22.1.2 ruc-powerdns:5.1.3
-docker run -d --name ruc-unbound --network ruc-test-net --ip 172.22.1.3 ruc-unbound:1.22.0
-docker run -d --name ruc-knot --network ruc-test-net --ip 172.22.1.4 ruc-knot:5.7.4
-docker run -d --name ruc-technitium --network ruc-test-net --ip 172.22.1.5 ruc-technitium:13.1
 
 echo "[*] Start containers of tested DNS resolvers for the RUC test, done."
