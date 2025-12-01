da371e2c9a99f6aaeee15b7713b98d6606bb334d
jtaeyeon05 <jtaeyeon05@gmail.com>
2025-12-02 05:21:06 +0900

    Fix: test_ruc_dnskey.sh

--- poc_scripts/test_ruc_ds.sh
+++ poc_scripts/test_ruc_ds.sh
@@ -14,18 +14,3 @@ sleep 5
 echo "Tesing BIND resolver against ruc_ds (w/ SIG)..."
 docker restart ruc-bind
 docker exec ruc-attacker python3 /root/poc_scripts/ruc_poc.py --resolver_ip 172.22.1.1 --ruc_variant ruc_ds --with_sig 1
-
-# test powerdns
-echo "Preparing nameserver..."
-docker restart ruc-nameserver
-sleep 5
-echo "Tesing PowerDNS resolver against ruc_ds (w/o SIG)..."
-docker restart ruc-powerdns
-docker exec ruc-attacker python3 /root/poc_scripts/ruc_poc.py --resolver_ip 172.22.1.2 --ruc_variant ruc_ds --with_sig 0
-
-echo "Preparing nameserver..."
-docker restart ruc-nameserver
-sleep 5
-echo "Tesing PowerDNS resolver against ruc_ds (w/ SIG)..."
-docker restart ruc-powerdns
-docker exec ruc-attacker python3 /root/poc_scripts/ruc_poc.py --resolver_ip 172.22.1.2 --ruc_variant ruc_ds --with_sig 1
